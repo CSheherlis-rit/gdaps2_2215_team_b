1248f263347f3c4b0a20bcd2cd35b7072330132f
johncderstine@gmail.com <johncderstine@gmail.com>
2022-02-28 14:07:31 -0500

    TileMap setup started

--- game/Puck Duck/Puck Duck/TileMap.cs
+++ game/Puck Duck/Puck Duck/TileMap.cs
@@ -6,5 +6,24 @@ namespace Puck_Duck
 {
     class TileMap
     {
+        //fields
+
+        //properties
+
+        //constructor
+        public TileMap()
+        {
+
+        }
+
+        //methods
+        //creates multi-dimensional array of tiles
+        //NOTE THIS WILL BE REPLACED WITH A DATA DRIVEN VERSION
+        //THAT TAKES TILE TYPES LIKE WALLS AND PISTIONS AND LOADS THEM
+        //FROM FILE
+        public Tile[][] GenerateTileMap()
+        {
+
+        }
     }
 }

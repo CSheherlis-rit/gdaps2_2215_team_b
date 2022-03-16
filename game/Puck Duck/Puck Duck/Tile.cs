758c69eb707ee159e849fb2d53b0817dabd734cc
EnterAnCode <69180263+EnterAnCode@users.noreply.github.com>
2022-03-16 00:15:27 -0400

    file IO frameworks

--- game/Puck Duck/Puck Duck/Tile.cs
+++ game/Puck Duck/Puck Duck/Tile.cs
@@ -1,27 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace Puck_Duck
 {
+    // enumeration to say what type of tile the current one is
+    enum Type
+    {
+        Empty,
+        Wall,
+        Piston,
+        Goal,
+        Start
+    }
+
     class Tile
     {
-        //type is what kind of tile it is
-        // 0 = empty
-        // 1 = wall
-        // 2 = piston
-        // 3 = goal
-        private int type;
+        private Type type;
+        private Rectangle position;
 
         private const int width = 32;
         private const int height = 32;
 
-        public int Type
+        public Type Type
         {
             get { return type; }
         }
 
-        public Tile(int type)
+        public Rectangle Position
+        {
+            get { return position; }
+            set { position = value; }
+        }
+
+        public Tile(Type type)
         {
             this.type = type;
         }
